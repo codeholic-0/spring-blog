8dad90ad82b544bea3ca47ee17fbb781cbbccfe1
Debankan Roy <userx4006@gmail.com>
2026-06-04 00:17:07 +0530

    Fixed the BlogApplication.java position

--- backend/blog-web/src/main/java/com/blog/BlogApplication.java
+++ backend/blog-web/src/main/java/com/blog/BlogApplication.java
@@ -1,9 +1,9 @@
-package com.blog.web;
+package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.blog")
+@SpringBootApplication
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
